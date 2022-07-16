1c80384e5a99693a01fab961b124ef9f3b1652f3
Aditya Halder <100075008+adityahalderxd@users.noreply.github.com>
2022-07-16 14:04:54 +0530

    Update global.py

--- AdityaHalder/plugins/global.py
+++ AdityaHalder/plugins/global.py
@@ -10,7 +10,7 @@ from pyrogram.types import (
     InlineQueryResultArticle,
     InputTextMessageContent,
     Message)
-from AdityaHalder.utilities.mongo import kaalub_info, rkaal, runkaal, loveub_info, rlove, runlove 
+from AdityaHalder.utilities.mongo import gban_info, gban_list, gban_user, ungban_user, kaalub_info, rkaal, runkaal, loveub_info, rlove, runlove 
 from AdityaHalder.utilities.data import *
 from AdityaHalder.config import *
 from AdityaHalder.modules.helpers.filters import *
@@ -19,6 +19,81 @@ from AdityaHalder.modules.databases.gmute_db import get_gmuted_users, gmute_user
 from AdityaHalder.modules.databases.gban_db import *
 from AdityaHalder.modules.helpers.program import get_arg
 from AdityaHalder.modules.helpers.admins import CheckAdmin
+
+
+
+async def iter_chats(client: Client):
+    """Iter Your All Chats"""
+    chats = []
+    async for dialog in client.iter_dialogs():
+        if dialog.chat.type in ["supergroup", "channel"]:
+            chats.append(dialog.chat.id)
+    return chats
+
+def get_user(message: Message, text: str) -> [int, str, None]:
+    """Get User From Message"""
+    if text is None:
+        asplit = None
+    else:
+        asplit = text.split(" ", 1)
+    user_s = None
+    reason_ = None
+    if message.reply_to_message:
+        user_s = message.reply_to_message.from_user.id
+        reason_ = text if text else None
+    elif asplit is None:
+        return None, None
+    elif len(asplit[0]) > 0:
+        if message.entities:
+            if len(message.entities) == 1:
+                required_entity = message.entities[0]
+                if required_entity.type == "text_mention":
+                    user_s = int(required_entity.user.id)
+                else:
+                    user_s = int(asplit[0]) if asplit[0].isdigit() else asplit[0]
+        else:
+            user_s = int(asplit[0]) if asplit[0].isdigit() else asplit[0]
+        if len(asplit) == 2:
+            reason_ = asplit[1]
+    return user_s, reason_
+
+
+async def edit_or_send_as_file(
+    text: str,
+    message: Message,
+    client: Client,
+    caption: str = "`Result!`",
+    file_name: str = "result",
+    parse_mode="md",
+):
+    """Send As File If Len Of Text Exceeds Tg Limit Else Edit Message"""
+    if not text:
+        await message.edit("`Wait, What?`")
+        return
+    if len(text) > 1024:
+        await message.edit("`OutPut is Too Large, Sending As File!`")
+        file_names = f"{file_name}.text"
+        open(file_names, "w").write(text)
+        await client.send_document(message.chat.id, file_names, caption=caption)
+        await message.delete()
+        if os.path.exists(file_names):
+            os.remove(file_names)
+        return
+    else:
+        return await message.edit(text, parse_mode=parse_mode)
+
+def get_text(message: Message) -> [None, str]:
+    """Extract Text From Commands"""
+    text_to_return = message.text
+    if message.text is None:
+        return None
+    if " " in text_to_return:
+        try:
+            return message.text.split(None, 1)[1]
+        except IndexError:
+            return None
+    else:
+        return None
 
 # Aditya Halder
 
@@ -52,33 +127,82 @@ async def ungmute(app: Client, message):
     await message.edit(f"**Unmuted {get_user.first_name}, enjoy!**")
 
 @Client.on_message(filters.command("gban", ["."]) & filters.me)
-async def gban(app: Client, message):
-    reply = message.reply_to_message
-    if reply:
-        user = reply.from_user["id"]
-    else:
-        user = get_arg(message)
-        if not user:
-            await message.edit("**Whome should I gban?**")
-            return
-    get_user = await app.get_users(user)
-    await gban_user(get_user.id)
-    await message.edit(f"**Successfully Gbanned {get_user.first_name}!**")
+async def gbun_him(client: Client, message: Message):
+    gbun = await message.edit_text("`Processing..`")
+    text_ = get_text(message)
+    user, reason = get_user(message, text_)
+    failed = 0
+    if not user:
+        await gbun.edit("`Reply To User Or Mention To GBan Him`")
+        return
+    try:
+        userz = await client.get_users(user)
+    except:
+        await gbun.edit(f"`404 : User Doesn't Exists In This Chat !`")
+        return
+    if not reason:
+        reason = "Private Reason!"
+    mee= await client.get_me()
+    if userz.id == mee.id:
+        await gbun.edit("`Why bothering yourself`")
+        return
+    if await gban_info(userz.id):
+        await gbun.edit("`Re-Gban? Seriously? :/`")
+        return
+    await gbun.edit("`Please, Wait Fectching Your Chats!`")
+    chat_dict = await iter_chats(client)
+    chat_len = len(chat_dict)
+    if not chat_dict:
+        gbun.edit("`You Have No Chats! So Sad`")
+        return
+    await gbun.edit("`Starting GBans Now!`")
+    for devils in chat_dict:
+        try:
+            await client.kick_chat_member(devils, int(userz.id))
+        except:
+            failed += 1
+    await gban_user(userz.id, reason)
+    gbanned = f"**#GBanned** \n**User :** [{userz.first_name}](tg://user?id={userz.id}) \n**Reason :** `{reason}` \n**Affected Chats :** `{chat_len-failed}`"
+    await gbun.edit(gbanned)
+    
 
 
-@Client.on_message(filters.command("ungban", ["."]) & filters.me)
-async def ungban(app: Client, message):
-    reply = message.reply_to_message
-    if reply:
-        user = reply.from_user["id"]
-    else:
-        user = get_arg(message)
-        if not user:
-            await message.edit("**Whome should I ungban?**")
-            return
-    get_user = await app.get_users(user)
-    await ungban_user(get_user.id)
-    await message.edit(f"**Ungbanned {get_user.first_name}, enjoy!**")
+@Client.on_message(filters.me & command("ungban"))
+async def ungbun_him(client: Client, message: Message):
+    ungbun= await message.edit_text("`Processing..`")
+    text_ = get_text(message)
+    user = get_user(message, text_)[0]
+    failed = 0
+    if not user:
+        await ungbun.edit("`Reply To User Or Mention To Un-GBan Him`")
+        return
+    try:
+        userz = await client.get_users(user)
+    except:
+        await ungbun.edit(f"`404 : User Doesn't Exists!`")
+        return
+    mee= await client.get_me()
+    if userz.id == mee.id:
+        await ungbun.edit("`what a joke`")
+        return
+    if not await gban_info(userz.id):
+        await ungbun.edit("`Un-Gban A Ungbanned User? Seriously? :/`")
+        return
+    await ungbun.edit("`Please, Wait Fectching Your Chats!`")
+    chat_dict = await iter_chats(client)
+    chat_len = len(chat_dict)
+    if not chat_dict:
+        ungbun.edit("`You Have No Chats! So Sad`")
+        return
+    await ungbun.edit("`Starting Un-GBans Now!`")
+    for devils in chat_dict:
+        try:
+            await client.unban_chat_member(devils, int(userz.id))
+        except:
+            failed += 1
+    await ungban_user(userz.id)
+    ungbanned = f"**#Un_GBanned** \n**User :** [{userz.first_name}](tg://user?id={userz.id}) \n**Affected Chats :** `{chat_len-failed}`"
+    await ungbun.edit(ungbanned)
 
 
 @Client.on_message(filters.group & filters.incoming)
@@ -95,21 +219,6 @@ async def check_and_del(app: Client, message):
         await app.delete_messages(message.chat.id, message_id)
     except:
         pass
-
-@Client.on_message(filters.group & filters.incoming)
-async def check_and_del_(app: Client, message):
-    if not message:
-        return
-    try:
-        if not message.from_user.id in (await get_gban_users()):
-            return
-    except AttributeError:
-        return
-    try:
-        await app.kick_chat_member(chat_id=message.chat.id, user_id=message.from_user.id)
-    except:
-        pass
-
 
 
 @Client.on_message( ~filters.me & filters.incoming)
@@ -137,6 +246,23 @@ async def watch(client: Client, message: Message):
             await message.reply_text(love)
         except:
             return
+    if await gban_info(user):
+        if message.chat.type != "supergroup":
+            return
+        try:
+            me_ = await message.chat.get_member(int(client.me.id))
+        except:
+            return
+        if not me_.can_restrict_members:
+            return
+        try:
+            await client.kick_chat_member(message.chat.id, int(user))
+        except:
+            return
+        await client.send_message(
+            message.chat.id,
+            f"**#GbanWatch** \n**Chat ID :** `{message.chat.id}` \n**User :** `{user}` \n**Reason :** `{await gban_info(user)}`",
+        )
 
 
 @Client.on_message(command("gcast"))
