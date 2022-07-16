44f0c3cb4fe742c1b7049b4986b5577af5b8b84c
Aditya Halder <100075008+adityahalderxd@users.noreply.github.com>
2022-07-16 23:38:48 +0530

    Update __init__.py

--- AdityaHalder/utilities/__init__.py
+++ AdityaHalder/utilities/__init__.py
@@ -2,4 +2,4 @@ import motor.motor_asyncio
 from AdityaHalder.config import *
 
 mongo_dbb = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DB_URL)
-dbb = mongo_dbb["SPAMBOT"]
+dbb = mongo_dbb["GENIUSDB"]
