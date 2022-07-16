f8b9d44a01bec39cf5e95eac322b532c0c3b0ce1
Aditya Halder <100075008+adityahalderxd@users.noreply.github.com>
2022-07-16 20:03:33 +0530

    Update history.py

--- AdityaHalder/plugins/history.py
+++ AdityaHalder/plugins/history.py
@@ -9,7 +9,7 @@ from AdityaHalder.modules.helpers.command import commandpro
 from AdityaHalder.modules.helpers.decorators import sudo_users_only, errors
 
 
-@Client.on_message(command([".history"]))
+@Client.on_message(command(["history"]))
 @errors
 @sudo_users_only
 async def user_history(client: Client, message: Message):
