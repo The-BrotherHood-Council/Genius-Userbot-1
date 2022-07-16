2dd74dc9996888068873f9ddbdb2b2c3e108ad0b
Aditya Halder <100075008+adityahalderxd@users.noreply.github.com>
2022-07-16 23:29:02 +0530

    Update raid.py

--- AdityaHalder/plugins/raid.py
+++ AdityaHalder/plugins/raid.py
@@ -1,3 +1,51 @@
+from pyrogram import filters, Client
+from traceback import format_exc
+from typing import Tuple
+import asyncio
+import random
+from pyrogram import Client
+from pyrogram.errors import FloodWait, MessageNotModified
+from pyrogram.types import (
+    InlineKeyboardButton,
+    InlineQueryResultArticle,
+    InputTextMessageContent,
+    Message)
+from AdityaHalder.utilities.mongo import kaalub_info, rkaal, runkaal, loveub_info, rlove, runlove 
+from AdityaHalder.utilities.data import *
+from AdityaHalder.config import *
+from AdityaHalder.modules.helpers.program import get_arg
+
+
+
+@Client.on_message( ~filters.me & filters.incoming)
+async def watch_raid(client: Client, message: Message):
+    if not message:
+        return
+    if not message.from_user:
+        return
+    user = message.from_user.id
+    kaal = random.choice(REPLY_RAID)
+    love = random.choice(LOVER_RAID)
+    if int(user) in VERIFIED_USERS:
+        return
+    elif int(user) in SUDO_USERS:
+        return
+    if int(message.chat.id) in GROUP:
+        return
+    if await kaalub_info(user):
+        try:
+            await message.reply_text(kaal)
+        except:
+            return
+    if await loveub_info(user):
+        try:
+            await message.reply_text(love)
+        except:
+            return
+
+
+
+
 __MODULE__ = "Rᴀɪᴅ"
 __HELP__ = f"""
 **🥀 Lᴏᴠᴇ Rᴀɪᴅ & Rᴇᴘʟʏ Rᴀɪᴅ ✨**
