df637d3dc8ba11db6debf24b54afa74e5dd3568c
Aditya Halder <100075008+adityahalderxd@users.noreply.github.com>
2022-07-17 10:00:58 +0530

    Update config.py

--- AdityaHalder/config.py
+++ AdityaHalder/config.py
@@ -16,7 +16,7 @@ BOT_TOKEN = getenv("BOT_TOKEN", "5498692165:AAHEBZ_Ch0BW51erHnDNDbZJNqAWpnDQ-3Q"
 STRING_SESSION = getenv("STRING_SESSION", "session")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", ". ! /").split())
 MONGO_DB_URL = getenv("MONGO_DB_URL", "mongodb+srv://BOT34:BOT34@cluster0.2dh9k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-OWNER_ID = int(getenv("OWNER_ID", "5336023580"))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "5336023580").split()))
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001621569381"))
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5356564375").split()))
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/adityahalderxd/Genius-Userbot")
