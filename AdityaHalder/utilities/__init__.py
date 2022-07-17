ff6832d6148fc8de538a95609a18cc17d3bfd3cd
Aditya Halder <100075008+adityahalderxd@users.noreply.github.com>
2022-07-17 10:19:41 +0530

    Update __init__.py

--- AdityaHalder/utilities/__init__.py
+++ AdityaHalder/utilities/__init__.py
@@ -2,10 +2,10 @@ from motor.motor_asyncio import AsyncIOMotorClient as _mongo_client_
 from pymongo import MongoClient
 from pyrogram import Client
 from AdityaHalder import config
-from ..logger import LOGGER
+from .logger import LOGGER
 
 
-_mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
+_mongo_async_ = _mongo_client_(config.MONGO_DB_URL)
 _mongo_sync_ = MongoClient(config.MONGO_DB_URL)
 
 mongodb = _mongo_async_.Genius
